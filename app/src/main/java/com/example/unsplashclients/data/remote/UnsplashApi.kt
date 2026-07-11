84e91470e180ad95cd5201b204b516059c6075e2
John-Thailand <naoki.honda@mirrorfit.jp>
2026-07-11 13:02:26 +0900

    Retrofitインターフェースに新たなメソッドを追加

--- app/src/main/java/com/example/unsplashclients/data/remote/UnsplashApi.kt
+++ app/src/main/java/com/example/unsplashclients/data/remote/UnsplashApi.kt
@@ -3,10 +3,15 @@ package com.example.unsplashclients.data.remote
 import com.example.unsplashclients.common.Constants
 import retrofit2.http.GET
 import retrofit2.http.Headers
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface UnsplashApi {
     @Headers("Authorization: Client-ID ${Constants.API_KEY}")
     @GET("search/photos")
     suspend fun searchPhotos(@Query("query") query: String): SearchPhotosResultDto
+
+    @Headers("Authorization: Client-ID ${Constants.API_KEY}")
+    @GET("photos/{id}")
+    suspend fun getPhotoById(@Path("id") photoId: String): PhotoDetailDto
 }
