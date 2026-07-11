51b7362adc671401e016cd0f34d48283b08b3b53
John-Thailand <naoki.honda@mirrorfit.jp>
2026-07-11 13:08:20 +0900

    リポジトリ側も新しいメソッドを追加

--- app/src/main/java/com/example/unsplashclients/data/repository/PhotoRepositoryImpl.kt
+++ app/src/main/java/com/example/unsplashclients/data/repository/PhotoRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.example.unsplashclients.data.repository
 
+import com.example.unsplashclients.data.remote.PhotoDetailDto
 import com.example.unsplashclients.data.remote.SearchPhotosResultDto
 import com.example.unsplashclients.data.remote.UnsplashApi
 import com.example.unsplashclients.domain.repository.PhotoRepository
@@ -14,5 +15,9 @@ class PhotoRepositoryImpl @Inject constructor(
 ) : PhotoRepository {
     override suspend fun searchPhotos(query: String): SearchPhotosResultDto {
         return api.searchPhotos(query)
+    }
+
+    override suspend fun getPhotoById(photoId: String): PhotoDetailDto {
+        return api.getPhotoById(photoId)
     }
 }
--- app/src/main/java/com/example/unsplashclients/domain/repository/PhotoRepository.kt
+++ app/src/main/java/com/example/unsplashclients/domain/repository/PhotoRepository.kt
@@ -1,7 +1,9 @@
 package com.example.unsplashclients.domain.repository
 
+import com.example.unsplashclients.data.remote.PhotoDetailDto
 import com.example.unsplashclients.data.remote.SearchPhotosResultDto
 
 interface PhotoRepository {
     suspend fun searchPhotos(query: String): SearchPhotosResultDto
+    suspend fun getPhotoById(photoId: String): PhotoDetailDto
 }
