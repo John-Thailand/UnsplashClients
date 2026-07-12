0e8ebabca3c1b4be7a8e846dc17ab4882df4a168
John-Thailand <naoki.honda@mirrorfit.jp>
2026-07-12 13:47:26 +0900

    ローディング表示とエラー表示

--- app/src/main/java/com/example/unsplashclients/presentation/search_photos/SearchPhotosScreen.kt
+++ app/src/main/java/com/example/unsplashclients/presentation/search_photos/SearchPhotosScreen.kt
@@ -1,11 +1,17 @@
 package com.example.unsplashclients.presentation.search_photos
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import com.example.unsplashclients.presentation.search_photos.components.PhotoThumbnail
 import com.example.unsplashclients.presentation.search_photos.components.SearchBar
@@ -25,12 +31,28 @@ fun SearchPhotosScreen(
             )
         }
     ) { paddingValue ->
-        LazyColumn(modifier = Modifier.padding(paddingValue)) {
-            items(state.photos) { photo ->
-                PhotoThumbnail(
-                    photo = photo,
-                    onClick = {}
-                )
+        Box(modifier = Modifier.fillMaxSize()) {
+            when {
+                state.isLoading -> {
+                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+                }
+                !state.error.isNullOrBlank() -> {
+                    Text(
+                        text = state.error,
+                        modifier = Modifier.align(Alignment.Center),
+                        color = Color.Red
+                    )
+                }
+                else -> {
+                    LazyColumn(modifier = Modifier.padding(paddingValue)) {
+                        items(state.photos) { photo ->
+                            PhotoThumbnail(
+                                photo = photo,
+                                onClick = {}
+                            )
+                        }
+                    }
+                }
             }
         }
     }
